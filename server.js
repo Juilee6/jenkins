7e0f507c6a1d0c666c0da276e511dc248508f074
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2021-10-25 10:57:44 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world welcome to my website\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
